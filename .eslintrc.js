3840666dc50f3c17e11fd47d79293a370f685148
David Moseler <davidmoseler@protonmail.com>
2020-06-10 12:00:40 -0300

    Tweak eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,42 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-plusplus': 0,
+    eqeqeq: 0,
+    'no-underscore-dangle': 0,
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true
+      }
+    ]
   },
+
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
-        jest: true,
-      },
+        jest: true
+      }
     },
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
   ],
+
+  extends: ['plugin:vue/essential', '@vue/airbnb', 'eslint:recommended', '@vue/prettier']
 };
