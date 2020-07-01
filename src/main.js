2b908252f0e89bd3d74f149e6f6c64ff0bcfc506
David Moseler <davidmoseler@protonmail.com>
2020-07-01 00:54:03 -0300

    Add vue session

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import VueSession from 'vue-session'
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueSession, {persist: true})
 
 new Vue({
   router,
